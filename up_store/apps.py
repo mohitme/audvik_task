4d9b3d8914f543728d5c2fb10cc3bb9ebcd26d31
Mohit Kumar <mohitme@outlook.com>
2020-08-13 11:08:14 +0530

    added file model

---
+++ up_store/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class UpStoreConfig(AppConfig):
+    name = 'up_store'
---
+++ up_store/models.py
@@ -0,0 +1,8 @@
+from django.db import models
+from django.conf import settings
+# Create your models here.
+
+class csv_file(models.Model):
+    description = models.CharField(max_length=255, blank=True)
+    document = models.FileField(upload_to=settings.MEDIA_ROOT)
+    uploaded_at = models.DateTimeField(auto_now_add=True)
